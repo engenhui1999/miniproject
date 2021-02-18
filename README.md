488b2a572453a64945b4f1162b2a2a2072775234
Immanuelle <ngfei25@gmail.com>
2021-02-18 22:30:05 +0700

    Helped formatting ;)

---
+++ README.md
@@ -0,0 +1,29 @@
+# SHRO Face Recognition Project
+This is a simple face recognition project for us during the SHRO period. Our goal is to create a
+state of the art face recognition system, researching on the latest trends andbest practices. All
+in the unit are welcome to join :)
+
+## How to start:
+The project runs on a 2 step process. The face detector focuses on detecting faces on a loop, while
+the face recognition will only run when the button is pressed. Press the 'Get Info' to get the stored 
+information about the person.  
+
+To run the Face Detector:  
+```
+python face_detector.py
+```
+To run the Recognition App:  
+```
+python recognition.py
+```
+
+## Dependencies
+To use GPU, please download tensorflow-gpu of the same version, software required:  
+CUDA >= 11.0  
+cuDNN >= 8.0  
+python >= 3.6  
+
+## Contributors:
+Yu Fei :) i helped  
+Grandmaster Wayne (rated 2833 in chess)  
+Hao Yi :O i helped too
--- face_detector.py
+++ face_detector.py
@@ -10,35 +10,6 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 video_capture = cv2.VideoCapture(0)
 anterior = 0
 
-
-def face_checker(n):
-    model = tensorflow.keras.models.load_model('keras_model.h5')
-
-    # Create the array of the right shape to feed into the keras model
-    # The 'length' or number of images you can put into the array is
-    # determined by the first position in the shape tuple, in this case 1.
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
-    # Replace this with the path to your image
-    image_array = np.asarray(n)
-    image_array = np.array([image_array, image_array, image_array])
-    image_array = image_array.reshape(224, 224, 3)
-
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # run the inference
-    prediction = model(data)
-    print(prediction)
-    if prediction[0, 0] > 0.8:
-        return print("Hello En Hui")
-    elif prediction[0, 1] > 0.8:
-        return print("Hello Bottle")
-
-
 while True:
     if not video_capture.isOpened():
         print('Unable to load camera.')
@@ -61,14 +32,16 @@ while True:
         cv2.rectangle(frame, (x-10, y-10), (x+w+10, y+h+10), (200, 255, 0), 2)
         roi = frame[y-10:y + h + 10, x - 10:x + w + 10]
         if roi is not [] and x > 10 and y > 10:
-            cv2.imshow('Bounding Box', roi)
             face_detected = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
             face_detected = Image.fromarray(face_detected)
             face_detected = face_detected.resize((224, 224))
-            face_checker(face_detected)
+            cv2.imshow('Bounding Box', np.array(face_detected))
+            face_detected.save('faces/face.jpg')
+
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
+
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
